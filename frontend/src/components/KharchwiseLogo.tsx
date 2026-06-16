d26af796f5e9edb2992cfe77a6e29119e5157523
Ankan Ghosh <ghoshankan005@gmail.com>
2026-06-16 14:07:35 +0530

    style: professionally increase logo scale and container height

--- frontend/src/components/KharchwiseLogo.tsx
+++ frontend/src/components/KharchwiseLogo.tsx
@@ -4,10 +4,10 @@ interface Props {
 }
 
 const KharchwiseLogo = ({ size = 'sm' }: Props) => {
-  const heightClass = size === 'lg' ? 'h-12' : 'h-7';
+  const heightClass = size === 'lg' ? 'h-16' : 'h-9';
   return (
     <div className={`flex items-center justify-center ${heightClass}`}>
-      <img src="/logo.png" alt="Kharchwise" className="h-full w-auto object-contain scale-[1.4]" />
+      <img src="/logo.png" alt="Kharchwise" className="h-full w-auto object-contain scale-[1.6]" />
     </div>
   );
 };
